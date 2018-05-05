808f971fd9642fd99dbcff69ae8672ddbbdc7adf
harshil <harshilshah4251@gmail.com>
2018-05-05 12:42:00 -0400

    updated the tool to pre-populate the fields with selected text

---
+++ README.md
@@ -0,0 +1,37 @@
+# Vocabulary Builder
+This utility assists in compiling a list of useful vocabulary in an efficient and simple manner. For example, if you are reading something on internet and you come across a useful vocabulary term, then you can press the assigned keyboard shortcut and add the term and its meaning automatically to your vocabulary list in a matter of seconds. This vocabulary list can be useful in preparing for standardized tests, improving communication and written skills, and for building your own personal dictionary. 
+
+### Files included:
+
+dictionary.json : This file contains the words that have been compiled together overtime by the user in a JSON format.
+vocab_list.pdf : This file uses dicitionary.json to create a readable PDF file.
+vocab_builder.py: This file contains the logic that gets the word and its definition and puts it into the dictionary.json file.
+
+### API's used:
+
+PyDictionary: for getting the definition of the word
+PyQt: for designing the GUI for the application
+
+### How to use this utility:
+1. First, open up vocab_builder.py and change the paths of the file to suit your needs.
+
+2. Use PyInstaller to create an executable python file. The executable python file will be stored in the dist directory in your root project folder.
+
+3. Using the linux terminal, copy over the generated executable to usr/local/bin.
+
+4. Open linux settings->devices->keyboard --> add new shortcut
+
+5. Name field is arbitrary. For the command field, insert "vocab_builder" (without the quotes. Note: This name must be same as the name of the executable in usr/local/bin)
+
+6. Assign any shortcut key combination.
+
+### Code in action:
+
+Select the word that you want to add to your list of words.
+Press the shortcut key.
+This should open up a dialog box populated with your selected word and its definition.
+You can view your own list of words and its definitions by opening the vocab_list.pdf file.
+
+
+### Extra Notes:
+A sample of words list is provided along with other files. Delete all the words keeping just the {}. This is required otherwise the program will throw an error.
---
+++ vocab_builder.py
@@ -0,0 +1,162 @@
+#!/usr/bin/python3
+import sys
+import os
+import json
+from PyQt5.Qt import QApplication, QClipboard
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QWidget, QPlainTextEdit, QLineEdit, QLabel, QPushButton
+from PyQt5.QtCore import QSize
+from PyDictionary import PyDictionary
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.enums import TA_JUSTIFY
+from reportlab.lib.enums import TA_CENTER
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch
+
+class ClipBoardWindow(QMainWindow):
+    dictionary = PyDictionary()
+
+    def __init__(self, selected_text):
+        QMainWindow.__init__(self)
+        self.setMinimumSize(QSize(440, 240))    
+        self.setWindowTitle("Vocab Builder") 
+        self.word_label = QLabel(self)
+        self.word_label.setText('Word : ')
+        self.word_label.move(20, 20)
+        # Add word_text field
+        self.word_box = QLineEdit(self)
+        self.word_box.move(100,20)
+        self.word_box.resize(280, 40)
+        #definition_label
+        self.def_label = QLabel(self)
+        self.def_label.setText('Definition : ')
+        self.def_label.move(20, 100)
+        #definition_text
+        self.def_box = QPlainTextEdit(self)
+        self.def_box.move(100,100)
+        self.def_box.resize(280, 80)
+        #button
+        self.submit_button = QPushButton('Submit', self)
+        self.submit_button.move(180, 200)  
+        self.submit_button.clicked.connect(self.on_click_submit)
+
+        #get selected text and meaning
+        self.selected_text = selected_text.lower()
+        self.word_box.setText(self.selected_text)
+        meaning = self.getMeaning(self.selected_text)
+        self.def_box.clear()
+        self.def_box.insertPlainText(meaning)
+        #QApplication.clipboard().dataChanged.connect(self.clipboardChanged)
+
+    def getNoun(self, text):
+        noun_array = []
+        if "Noun" in ClipBoardWindow.dictionary.meaning(text):
+            noun_array = ClipBoardWindow.dictionary.meaning(text)["Noun"]
+            noun_string = ""
+            for noun in noun_array:
+                noun_string += noun + '; '
+            return noun_string[0: len(noun_string) - 2]
+        else:
+            return None
+        
+
+    def getVerb(self, text):
+        verb_array = []
+        if "Verb" in ClipBoardWindow.dictionary.meaning(text):
+            verb_array = ClipBoardWindow.dictionary.meaning(text)["Verb"]
+            verb_string = ""
+            for verb in verb_array:
+                verb_string += verb + '; '
+            return verb_string[0: len(verb_string) - 2]        
+        else:
+            return None
+        
+
+    def getAdj(self, text):
+        adj_array = []
+        if "Adjective" in ClipBoardWindow.dictionary.meaning(text):
+            adj_array = ClipBoardWindow.dictionary.meaning(text)["Adjective"]
+            adj_string = ""
+            for adj in adj_array:
+                adj_string += adj + '; '
+            return adj_string[0: len(adj_string) - 2]
+        else:
+            return None
+        
+
+    # return the meaning of the word
+
+    def getMeaning(self, text):
+        noun = self.getNoun(text)
+        verb = self.getVerb(text)
+        adj = self.getAdj(text)
+        if noun != None:
+            return noun
+        elif verb != None:
+            return verb
+        elif adj != None:
+            return adj
+        else:
+            return "Definition not found"
+    
+
+
+    
+    # Get the system clipboard contents
+    # alternate way to populate the fields with the content copied to the clipboard
+    def clipboardChanged(self):
+        text = QApplication.clipboard().text().lower()
+        self.word_box.setText(text)
+        meaning = self.getMeaning(text)
+        self.def_box.clear()
+        self.def_box.insertPlainText(meaning)
+
+    def on_click_submit(self):
+        with open('/home/harshil/Harshil/projects/personal_projects/vocab_builder/dictionary.json', 'r+') as file:
+            data = json.load(file)
+            new_data = {
+                self.word_box.text(): self.def_box.toPlainText()
+            }
+            data.update(new_data)
+            file.seek(0)
+            json.dump(data, file, indent=4)
+            file.truncate()
+            PDFgenerator.generatePDF()
+            print("Successfully added the word and its definition")
+            sys.exit()
+
+
+class PDFgenerator():
+    @staticmethod
+    def generatePDF():
+        with open('/home/harshil/Harshil/projects/personal_projects/vocab_builder/dictionary.json', 'r') as file:
+            data = json.load(file)
+        doc = SimpleDocTemplate("/home/harshil/Harshil/projects/personal_projects/vocab_builder/vocab_list.pdf",pagesize=letter,
+                        rightMargin=18,leftMargin=18,
+                        topMargin=18,bottomMargin=18)
+        Story=[]
+        styles=getSampleStyleSheet()
+        styles.add(ParagraphStyle(name='custom_title', alignment=TA_CENTER, fontName = "Times-Roman"))
+        styles.add(ParagraphStyle(name='custom_content', alignment=TA_JUSTIFY, fontName = "Times-Roman"))
+        ptext = '<font size = 20 color = "green"> Vocab list </font>'
+        Story.append(Paragraph(ptext, styles["custom_title"]))
+        Story.append(Spacer(1, 10))
+        for word in data:
+            definition = data[word]
+            ptext = '<font size=8 color = "red">%s </font>: <font size = 8> %s </font>' % (word, definition)
+            Story.append(Paragraph(ptext, styles["custom_content"]))
+            Story.append(Spacer(1, 4))
+        doc.build(Story)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    selected_text = os.popen('xsel').read()
+    mainWin = ClipBoardWindow(selected_text)
+    mainWin.show()
+    sys.exit(app.exec())
+
+
+
